8a837f7bc57876b9c04c61ae0c04600ae541c1e1
Amit Alon <amit@kaholo.io>
2021-05-10 13:01:31 +0300

    small fix

--- src/app.js
+++ src/app.js
@@ -8,15 +8,15 @@ function _getsudo(params,settings){
 }
 
 async function exposeDeployment(action,settings) {
-  let params = action.params;
-  let type = params.TYPE || 'NodePort'
-  let execString = `${_getsudo(params,settings)} kubectl expose deployment ${params.DEPLOYMENT} --type=${type} --name=${params.NAME}`
+  const params = action.params;
+  const type = params.TYPE || 'NodePort'
+  const execString = `${_getsudo(params,settings)} kubectl expose deployment ${params.DEPLOYMENT} --type=${type} --name=${params.NAME}`
   return exec(execString);
 }
 
 async function getServices(action,settings) {
-  let params = action.params;
-  let execString = `${_getsudo(params,settings)} kubectl get svc -n ${action.params.namespace} -o json`
+  const params = action.params;
+  const execString = `${_getsudo(params,settings)} kubectl get svc -n ${action.params.namespace} -o json`
   const execResult = await exec(execString);
   try{
     return JSON.parse(execResult.stdout);
@@ -26,18 +26,18 @@ async function getServices(action,settings) {
 }
 
 async function getPods(action,settings){
-  let SUDO = _getsudo(action.params,settings)
-  let execString = ` ${SUDO} kubectl get pod`
-  let res = await exec(execString);
+  const SUDO = _getsudo(action.params,settings)
+  const execString = ` ${SUDO} kubectl get pod`
+  const res = await exec(execString);
   if(!res.stdout.includes('READY')){
     return Promise.resolve(res);
   }
   console.log(res.stdout)
-  let spaces = /\s\s+/g;
-  let resultsArr = res.stdout.split('\n',res.stdout.replace(/[^'\n']/g, "").length)
-  let titles = resultsArr[0].split(spaces)
-  let newResult = []
-  let newRow = {}
+  const spaces = /\s\s+/g;
+  const resultsArr = res.stdout.split('\n',res.stdout.replace(/[^'\n']/g, "").length)
+  const titles = resultsArr[0].split(spaces)
+  const newResult = []
+  const newRow = {}
   for (let i = 1; i < resultsArr.length; i++) {
     const row = resultsArr[i].split(spaces);
     titles.forEach((title,index)=>{
@@ -52,11 +52,12 @@ async function getPods(action,settings){
 }
 
 async function apply(action,settings){
-  let SUDO = _getsudo(action.params,settings)
-  let force = action.params.FORCE ? '--force' : ''
-  let overwrite = action.params.OVERWRITE ? '--overwrite' : ''
-  let dryRun = action.params.DRY_RUN ? '--dry-run' : ''
-  let execString = `${SUDO} kubectl apply -f ${action.params.FILE_PATH}  ${force} ${overwrite} ${dryRun}`
+  const SUDO = _getsudo(action.params,settings);
+  const force = action.params.FORCE ? '--force' : '';
+  const noOverwrite = action.params.NO_OVERWRITE ? '--overwrite=false' : '';
+  const dryRun = action.params.DRY_RUN ? `--dry-run=${action.params.DRY_RUN.trim()}` : '';
+  const args = [force, noOverwrite, dryRun].filter(arg => arg);
+  const execString = `${SUDO} kubectl apply -f ${action.params.FILE_PATH} ${args.join(' ')}`;
   return exec(execString);
 }
 
